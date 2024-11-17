8b1b3d1047f68df47014ecb0f18e650e8991490b
Vitor Barcellos <vtor.barcellos7@gmail.com>
2024-11-17 15:29:33 -0300

    Added fly behaviour, flyer actor and AI flyer controller

---
+++ Assets/Code/Actors/Concrete/AiFlyerActor.cs
@@ -0,0 +1,62 @@
+﻿using Code.ActorControllers.Concrete;
+using Code.Actors.Abstraction;
+using Code.Behaviours.Abstraction;
+using Code.Behaviours.Concrete;
+using Code.Services.Abstraction;
+using Code.Services.ServiceLocator;
+using Code.Utils;
+using UnityEngine;
+
+namespace Code.Actors.Concrete
+{
+    public class AiFlyerActor : ControllableActor, IFlyerActor
+    {
+        [SerializeField] private CollisionObserver _killTriggerObserver;
+        [SerializeField] private AnimationCurve _horizontalMovementCurve;
+        [SerializeField] private float _horizontalCurveAmplitude;
+        [SerializeField] private float _horizontalCurveDuration;
+        [SerializeField] private AnimationCurve _verticalMovementCurve;
+        [SerializeField] private float _verticalCurveAmplitude;
+        [SerializeField] private float _verticalCurveDuration;
+
+        private IFlyBehaviour _flyBehaviour;
+        private IKillBehaviour _killBehaviour;
+        
+        protected override void InitBehaviours()
+        {
+            _flyBehaviour = new FlyBehaviour(transform);
+            _killBehaviour = new KillBehaviour();
+            
+            _killTriggerObserver.OnTriggerEnter += OnKillTriggerEnter;
+        }
+
+        protected override void BindController()
+        {
+            var tickService = ServiceLocator.Get<ITickService>();
+
+            _controller = new AiFlyerActorController(this, tickService, _horizontalMovementCurve,
+                _verticalMovementCurve, _horizontalCurveAmplitude,
+                _verticalCurveAmplitude, _horizontalCurveDuration,
+                _verticalCurveDuration);
+            _controller.SetEnabled(true);
+        }
+
+        public void UpdateMovement(Vector2 position)
+            => _flyBehaviour.UpdateMovement(position);
+
+        private void OnKillTriggerEnter(GameObject go)
+        {
+            var killableActor = go.GetComponentInParent<KillableActor>();
+
+            if (killableActor == null)
+                return;
+            
+            _killBehaviour.Kill(killableActor.KillableBehaviour);
+        }
+
+        protected override void DisposeBehaviours()
+        {
+            _killTriggerObserver.OnTriggerEnter -= OnKillTriggerEnter;
+        }
+    }
+}
---
+++ Assets/Code/Behaviours/Concrete/FlyBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using Code.Behaviours.Abstraction;
+using UnityEngine;
+
+namespace Code.Behaviours.Concrete
+{
+    public class FlyBehaviour : IFlyBehaviour
+    {
+        private readonly Transform _transform;
+
+        public FlyBehaviour(Transform transform)
+        {
+            _transform = transform;
+        }
+        
+        public void UpdateMovement(Vector2 delta)
+        {
+            var oldPos = _transform.position;
+            
+            var newPos = new Vector3(
+                oldPos.x + delta.x,
+                oldPos.y + delta.y,
+                oldPos.z);
+
+            _transform.position = newPos;
+        }
+    }
+}
