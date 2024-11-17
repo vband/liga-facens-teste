8692d0f1fc81dc52e3d1bb408bb2607459fa9e12
Vitor Barcellos <vtor.barcellos7@gmail.com>
2024-11-17 11:05:06 -0300

    Added LevelFinishedMenu and LevelFailedMenu

---
+++ Assets/Code/Menus/LevelFailed/LevelFailedMenu.cs
@@ -0,0 +1,39 @@
+﻿using Code.Services.Abstraction;
+using Code.Services.ServiceLocator;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Menus.LevelFailed
+{
+    public class LevelFailedMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject _canvas;
+        [SerializeField] private Button _restartLevelButton;
+
+        private ILevelFailedMenuService _levelFailedMenuService;
+        private ILevelService _levelService;
+
+        private void Start()
+        {
+            _canvas.SetActive(false);
+            
+            _levelFailedMenuService = ServiceLocator.Get<ILevelFailedMenuService>();
+            _levelService = ServiceLocator.Get<ILevelService>();
+            
+            _levelFailedMenuService.OnLevelFailedMenuInvoked += InvokeMenu;
+            
+            _restartLevelButton.onClick.AddListener(RestartLevel);
+        }
+
+        private void InvokeMenu()
+            => _canvas.SetActive(true);
+
+        private void RestartLevel()
+            => _levelService.RestartCurrentLevel();
+
+        private void OnDestroy()
+        {
+            _levelFailedMenuService.OnLevelFailedMenuInvoked -= InvokeMenu;
+        }
+    }
+}
---
+++ Assets/Code/Menus/LevelFinished/LevelFinishedMenu.cs
@@ -0,0 +1,46 @@
+﻿using Code.Services.Abstraction;
+using Code.Services.ServiceLocator;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Menus.LevelFinished
+{
+    public class LevelFinishedMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject _canvas;
+        [SerializeField] private Button _nextLevelButton;
+        [SerializeField] private Button _restartLevelButton;
+
+        private ILevelFinishedMenuService _levelFinishedMenuService;
+        private ILevelService _levelService;
+
+        private void Start()
+        {
+            _canvas.SetActive(false);
+            
+            _levelFinishedMenuService = ServiceLocator.Get<ILevelFinishedMenuService>();
+            _levelService = ServiceLocator.Get<ILevelService>();
+            
+            _levelFinishedMenuService.OnLevelFinishedMenuInvoked += InvokeMenu;
+            
+            _nextLevelButton.onClick.AddListener(GoToNextLevel);
+            _restartLevelButton.onClick.AddListener(RestartLevel);
+
+            _nextLevelButton.interactable = !_levelService.IsLastLevel;
+        }
+
+        private void InvokeMenu()
+            => _canvas.SetActive(true);
+
+        private void GoToNextLevel()
+            => _levelService.LoadNextLevel();
+
+        private void RestartLevel()
+            => _levelService.RestartCurrentLevel();
+
+        private void OnDestroy()
+        {
+            _levelFinishedMenuService.OnLevelFinishedMenuInvoked -= InvokeMenu;
+        }
+    }
+}
